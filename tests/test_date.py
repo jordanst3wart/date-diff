ecf55c96d691c1a95bbd4df7d08c9a040c7b2b8a
jordanst3wart <jordanstewart2428@gmail.com>
2021-01-12 22:44:49 +1100

    improve imports

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,159 +1,158 @@
-import lib.date as date
-import lib.leap_year as leap_year
+from lib.date import Date
 import unittest
 
 
 class TestDateValidation(unittest.TestCase):
     def test_invalid_before_date(self):
         with self.assertRaises(ValueError):
-            date.Date(1, 1, 1600)
+            Date(1, 1, 1600)
 
     def test_invalid_after_date(self):
         with self.assertRaises(ValueError):
-            date.Date(1, 1, 3001)
+            Date(1, 1, 3001)
 
     def test_invalid_day(self):
         with self.assertRaises(ValueError):
-            date.Date(32, 1, 2000)
+            Date(32, 1, 2000)
 
     def test_invalid_day2(self):
         with self.assertRaises(ValueError):
-            date.Date(-1, 1, 2000)
+            Date(-1, 1, 2000)
 
     def test_invalid_month(self):
         with self.assertRaises(ValueError):
-            date.Date(1, 13, 2000)
+            Date(1, 13, 2000)
 
     def test_invalid_day_month_combo(self):
         with self.assertRaises(ValueError):
-            date.Date(31, 4, 2000)
+            Date(31, 4, 2000)
 
     def test_leap_day_true(self):
-        date.Date(29, 2, 2000)
+        Date(29, 2, 2000)
 
     def test_leap_day_false(self):
         with self.assertRaises(ValueError):
-            date.Date(29, 2, 2001)
+            Date(29, 2, 2001)
 
 
 class TestDateDiff(unittest.TestCase):
     def test_diff_same_month_0(self):
-        date1 = date.Date(2, 1, 2000)
-        date2 = date.Date(1, 1, 2000)
+        date1 = Date(2, 1, 2000)
+        date2 = Date(1, 1, 2000)
         self.assertEqual(date1.diff(date2), 0)
 
     # test case given
     def test_diff_same_month_1(self):
-        date1 = date.Date(22, 6, 1983)
-        date2 = date.Date(2, 6, 1983)
+        date1 = Date(22, 6, 1983)
+        date2 = Date(2, 6, 1983)
         self.assertEqual(date1.diff(date2), 19)
 
     # test case given
     def test_diff_same_day(self):
-        date1 = date.Date(22, 6, 1983)
-        date2 = date.Date(22, 6, 1983)
+        date1 = Date(22, 6, 1983)
+        date2 = Date(22, 6, 1983)
         self.assertEqual(date1.diff(date2), 0)
 
     def test_diff_same_month_neg(self):
-        date1 = date.Date(2, 6, 1983)
-        date2 = date.Date(22, 6, 1983)
+        date1 = Date(2, 6, 1983)
+        date2 = Date(22, 6, 1983)
         self.assertEqual(date1.diff(date2), -19)
 
     def test_diff_diff_month(self):
-        date1 = date.Date(22, 7, 2022)
-        date2 = date.Date(2, 6, 2022)
+        date1 = Date(22, 7, 2022)
+        date2 = Date(2, 6, 2022)
         self.assertEqual(date1.diff(date2), 49)
 
     def test_diff_diff_month2(self):
-        date1 = date.Date(2, 7, 2022)
-        date2 = date.Date(22, 6, 2022)
+        date1 = Date(2, 7, 2022)
+        date2 = Date(22, 6, 2022)
         self.assertEqual(date1.diff(date2), 9)
 
     def test_diff_1year(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 6, 2022)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 6, 2022)
         self.assertEqual(date1.diff(date2), 364)
 
     def test_diff_2year(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 6, 2021)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 6, 2021)
         self.assertEqual(date1.diff(date2), 729)
 
     # test example 2
     def test_diff_example2(self):
-        date1 = date.Date(25, 12, 1984)
-        date2 = date.Date(4, 7, 1984)
+        date1 = Date(25, 12, 1984)
+        date2 = Date(4, 7, 1984)
         self.assertEqual(date1.diff(date2), 173)
 
     def test_diff_example3(self):
-        date1 = date.Date(3, 1, 1989)
-        date2 = date.Date(3, 8, 1983)
+        date1 = Date(3, 1, 1989)
+        date2 = Date(3, 8, 1983)
         self.assertEqual(date1.diff(date2), 1979)
 
     def test_diff_example3_neg(self):
-        date1 = date.Date(3, 8, 1983)
-        date2 = date.Date(3, 1, 1989)
+        date1 = Date(3, 8, 1983)
+        date2 = Date(3, 1, 1989)
         self.assertEqual(date1.diff(date2), -1979)
 
     def test_diff_leap_day(self):
-        date1 = date.Date(1, 3, 2000)
-        date2 = date.Date(28, 2, 2000)
+        date1 = Date(1, 3, 2000)
+        date2 = Date(28, 2, 2000)
         self.assertEqual(date1.diff(date2), 1)
 
     def test_diff_leap_day2(self):
-        date1 = date.Date(1, 3, 2000)
-        date2 = date.Date(29, 2, 2000)
+        date1 = Date(1, 3, 2000)
+        date2 = Date(29, 2, 2000)
         self.assertEqual(date1.diff(date2), 0)
 
 
 class TestMonthDiff(unittest.TestCase):
     def test_diff_diff_month(self):
-        date1 = date.Date(2, 7, 2022)
+        date1 = Date(2, 7, 2022)
         self.assertEqual(date1.__diff_month__(7, 6), 30)
 
     def test_diff_diff_month2(self):
-        date1 = date.Date(2, 8, 2022)
+        date1 = Date(2, 8, 2022)
         self.assertEqual(date1.__diff_month__(8, 6), 61)
 
     def test_diff_diff_month2_neg(self):
-        date1 = date.Date(2, 6, 2022)
+        date1 = Date(2, 6, 2022)
         self.assertEqual(-61, date1.__diff_month__(6, 8))
 
 
 class TestLessThanOrEqual(unittest.TestCase):
     def test_less_than_year(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 6, 2021)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 6, 2021)
         self.assertFalse(date1.less_than_or_equal(date2))
 
     def test_less_than_month(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 5, 2023)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 5, 2023)
         self.assertFalse(date1.less_than_or_equal(date2))
 
     def test_less_than_day(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(21, 6, 2023)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(21, 6, 2023)
         self.assertFalse(date1.less_than_or_equal(date2))
 
     def test_less_than_year_neg(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 6, 2025)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 6, 2025)
         self.assertTrue(date1.less_than_or_equal(date2))
 
     def test_less_than_month_neg(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 7, 2023)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 7, 2023)
         self.assertTrue(date1.less_than_or_equal(date2))
 
     def test_less_than_day_neg(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(23, 6, 2023)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(23, 6, 2023)
         self.assertTrue(date1.less_than_or_equal(date2))
 
     def test_less_than_equal(self):
-        date1 = date.Date(22, 6, 2023)
-        date2 = date.Date(22, 6, 2023)
+        date1 = Date(22, 6, 2023)
+        date2 = Date(22, 6, 2023)
         self.assertTrue(date1.less_than_or_equal(date2))
 
 
--- tests/test_leap_year.py
+++ tests/test_leap_year.py
@@ -1,99 +1,100 @@
 import lib.leap_year as leap_year
-import lib.date as date
+from lib.leap_year import is_leap_year, number_of_leap_days
+from lib.date import Date
 import unittest
 
 
 class TestIsLeapYear(unittest.TestCase):
     def test_is_not_leap_year(self):
-        self.assertFalse(leap_year.is_leap_year(2001))
+        self.assertFalse(is_leap_year(2001))
 
     def test_is_leap_year(self):
-        self.assertTrue(leap_year.is_leap_year(2004))
+        self.assertTrue(is_leap_year(2004))
 
     def test_is_leap_year_hundred(self):
-        self.assertFalse(leap_year.is_leap_year(1900))
+        self.assertFalse(is_leap_year(1900))
 
     def test_is_leap_year_four_hundred(self):
-        self.assertTrue(leap_year.is_leap_year(2000))
+        self.assertTrue(is_leap_year(2000))
 
 
 class TestLeapYearDays(unittest.TestCase):
     def test_leap_days_zero_day(self):
-        start_date = date.Date(1, 1, 2002)
-        end_date = date.Date(2, 1, 2002)
-        diff = leap_year.number_of_leap_days(start_date, end_date)
+        start_date = Date(1, 1, 2002)
+        end_date = Date(2, 1, 2002)
+        diff = number_of_leap_days(start_date, end_date)
         self.assertEqual(diff, 0)
 
     def test_leap_days_zero_month(self):
-        start_date = date.Date(1, 1, 2002)
-        end_date = date.Date(1, 2, 2002)
-        diff = leap_year.number_of_leap_days(start_date, end_date)
+        start_date = Date(1, 1, 2002)
+        end_date = Date(1, 2, 2002)
+        diff = number_of_leap_days(start_date, end_date)
         self.assertEqual(diff, 0)
 
     def test_leap_days_zero_year(self):
-        start_date = date.Date(1, 1, 2002)
-        end_date = date.Date(1, 1, 2003)
-        diff = leap_year.number_of_leap_days(start_date, end_date)
+        start_date = Date(1, 1, 2002)
+        end_date = Date(1, 1, 2003)
+        diff = number_of_leap_days(start_date, end_date)
         self.assertEqual(diff, 0)
 
     def test_leap_days_leap_day(self):
-        date1 = date.Date(1, 3, 2004)
-        date2 = date.Date(28, 2, 2004)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 3, 2004)
+        date2 = Date(28, 2, 2004)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 1)
 
     def test_leap_days_leap_month(self):
-        date1 = date.Date(28, 3, 2004)
-        date2 = date.Date(28, 1, 2004)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(28, 3, 2004)
+        date2 = Date(28, 1, 2004)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 1)
 
     def test_leap_days_leap_year(self):
-        date1 = date.Date(1, 1, 2005)
-        date2 = date.Date(1, 1, 2004)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2005)
+        date2 = Date(1, 1, 2004)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 1)
 
     def test_leap_days_leap_between_years(self):
-        date1 = date.Date(1, 1, 2007)
-        date2 = date.Date(1, 1, 2002)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2007)
+        date2 = Date(1, 1, 2002)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 1)
 
     def test_leap_days_leap_between_years2(self):
-        date1 = date.Date(1, 1, 2007)
-        date2 = date.Date(1, 9, 1999)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2007)
+        date2 = Date(1, 9, 1999)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 2)
 
     def test_leap_days_leap_starting_after(self):
-        date1 = date.Date(1, 1, 2002)
-        date2 = date.Date(1, 3, 2000)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2002)
+        date2 = Date(1, 3, 2000)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 0)
 
     def test_leap_days_leap_ending_before(self):
-        date1 = date.Date(28, 2, 2004)
-        date2 = date.Date(1, 11, 2000)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(28, 2, 2004)
+        date2 = Date(1, 11, 2000)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 0)
 
     def test_leap_days_leap_year_counting(self):
-        date1 = date.Date(1, 1, 2021)
-        date2 = date.Date(1, 1, 1966)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2021)
+        date2 = Date(1, 1, 1966)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 14)
 
     def test_leap_days_leap_year_multiple(self):
-        date1 = date.Date(1, 1, 2009)
-        date2 = date.Date(1, 1, 2004)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2009)
+        date2 = Date(1, 1, 2004)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 2)
 
     def test_leap_days_leap_year_multiple2(self):
-        date1 = date.Date(1, 1, 2009)
-        date2 = date.Date(1, 1, 2005)
-        diff = leap_year.number_of_leap_days(date1, date2)
+        date1 = Date(1, 1, 2009)
+        date2 = Date(1, 1, 2005)
+        diff = number_of_leap_days(date1, date2)
         self.assertEqual(diff, 1)
 
 
