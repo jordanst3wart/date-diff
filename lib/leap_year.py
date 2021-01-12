69b55987210b568e10ca07d195a78adff3fe6799
jordanst3wart <jordanstewart2428@gmail.com>
2021-01-12 15:51:40 +1100

    write leap years

--- lib/leap_year.py
+++ lib/leap_year.py
@@ -16,31 +16,48 @@ def is_leap_year(year: int):
 
 
 def number_of_leap_days(date1, date2):
-    """
-    Returns the number of leap days between the two dates
-    :param start_date:
-    :param end_date:
-    :return:
-    """
+    days = 0
+    # negative case
+    if date1 < date2:
+        return number_of_leap_days(date2, date1) * -1
+
+    days += full_leap_years(date1.year, date2.year)
+
+    if start_before_leap_day(date2):
+        days += 1
+    if end_after_leap_day(date1):
+        days += 1
+    if date1.year == date2.year and end_after_leap_day(date1) and start_before_leap_day(date2):
+        days -= 1
+    return days
 
 
-
-    if date1.year == date2.year and is_leap_year(date1.year):
-        return 0
-
-    # check if part years include feburary and are a leap year
-    # if year1 == year2 and year1 == leap_year
-    # check if months include feburary
-    # check if days include feburary
-
-    # iterate over years
-    range(date1.year, date2.year)
-
-    #date(1,2,200)
-    #if start_date
-    return 0
+# check for complete years
+def full_leap_years(year1, year2):
+    days = 0
+    if year1 - 3 > year2:
+        full_year1 = year1 - 1
+        full_year2 = year2 + 1
+        for i in range(full_year2, full_year1):
+            if is_leap_year(i):
+                days += 1
+    return days
 
 
-def is_leap_day(date):
-    pass
+def start_before_leap_day(date):
+    if is_leap_year(date.year):
+        if date.month < 2:
+            return True
+        elif date.month == 2 and date.day < 29:
+            return True
+        else:
+            return False
+
+
+def end_after_leap_day(date):
+    if is_leap_year(date.year):
+        if date.month > 2:
+            return True
+        else:
+            return False
 
